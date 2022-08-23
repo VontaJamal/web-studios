6153cf6e81999de1c83b30796972fa0c8db34684
Vonta Johnson <djwstech@gmail.com>
2022-08-23 07:47:49 -0400

    update heading

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -33,17 +33,18 @@ export default function Index() {
       <div className="py-10">
         <header>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">
-              Dashboard
+            <h1 className="text-center text-3xl font-bold leading-tight tracking-tight text-gray-900">
+              Web Studios
             </h1>
+            <h3 className="mt-3 text-center font-semibold leading-tight">
+              Web Development, Investing, and Career Tips!
+            </h3>
           </div>
         </header>
         <main>
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
             {/* <!-- Replace with your content --> */}
-            <div className="px-4 py-8 sm:px-0">
-              <div className="h-96 rounded-lg border-4 border-dashed border-gray-200"></div>
-            </div>
+
             {/* <!-- /End replace --> */}
           </div>
         </main>
