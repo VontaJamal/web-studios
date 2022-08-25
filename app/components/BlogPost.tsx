4a8a12fc53fb5621b47f9152c9d90dc2103f8d44
Vonta <vonta@dayslice.io>
2022-08-25 16:57:55 -0400

    add dagger border

--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -5,12 +5,22 @@ export default function BlogPost({ post }: { post: Post }) {
   console.log("post", post);
 
   return (
-    <div className="overflow-hidden rounded shadow-lg">
+    <div className=" rounded shadow-lg transition-all ease-in-out hover:border-2 hover:border-red-700 relative">
       <img
-        className="w-full"
-        src={post.image}
-        alt={`${post.title} cover`}
+        height={40}
+        width={40}
+        className="inline absolute -left-4 -top-14"
+        src="/assets/zidanedaggers.svg"
+        alt="zidaneee"
       />
+      <img
+        height={40}
+        width={40}
+        className="inline absolute -right-4 -bottom-14"
+        src="/assets/zidanedaggers.svg"
+        alt="zidaneee"
+      />
+      <img className="w-full" src={post.image} alt={`${post.title} cover`} />
       <div className="px-6 py-4">
         <div className="mb-2 text-2xl font-bold">{post.title}</div>
         <div className="px-6 pt-4 pb-2">
