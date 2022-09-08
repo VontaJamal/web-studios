07d0142eacb0dbaf2d0bf1192e24ad55f65d14a3
Vonta Johnson <djwstech@gmail.com>
2022-09-07 21:58:00 -0400

    adjust types and schema

--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -1,22 +1,22 @@
-import type { Post } from "@prisma/client";
+import type { Post } from "~/models/post.server";
 import Tag from "./Tag";
 
 export default function BlogPost({ post }: { post: Post }) {
   console.log("post", post);
 
   return (
-    <div className=" rounded shadow-lg transition-all ease-in-out hover:border-2 hover:border-red-700 relative">
+    <div className=" relative rounded shadow-lg transition-all ease-in-out hover:border-2 hover:border-red-700">
       <img
         height={40}
         width={40}
-        className="inline absolute -left-4 -top-14"
+        className="absolute -left-4 -top-14 inline"
         src="/assets/zidanedaggers.svg"
         alt="zidaneee"
       />
       <img
         height={40}
         width={40}
-        className="inline absolute -right-4 -bottom-14"
+        className="absolute -right-4 -bottom-14 inline"
         src="/assets/zidanedaggers.svg"
         alt="zidaneee"
       />
--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,14 @@
 import { prisma } from "~/db.server";
-import type { Post } from "@prisma/client";
-export type { Post };
+
+export type Post = {
+  slug: string;
+  title: string;
+  markdown: string;
+  image: string;
+  createdAt: Date;
+  updatedAt: Date;
+  tags: string[];
+};
 
 export async function getPosts() {
   return prisma.post.findMany();
@@ -10,6 +18,8 @@ export async function getPost(slug: string): Promise<Post> {
   return prisma.post.findUnique({ where: { slug } });
 }
 
-export async function createPost(post: Pick<Post, "slug" | "title" | "markdown">) {
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
   return prisma.post.create({ data: post });
 }
